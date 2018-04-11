43a5eb2af20163008ebe17e6ae14698c5f16feb0
Luke-Alexander <37579656+Luke-Alexander@users.noreply.github.com>
2018-04-11 13:01:22 +1000

    Add guitar_test.py

--- prac_07/guitar_test.py
+++ prac_07/guitar_test.py
@@ -1,4 +1,14 @@
 from prac_07.guitar import Guitar
+guitars = []
+print("My guitars!")
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
+    guitar = Guitar(name, year, cost)
+    guitars.append(guitar)
+    name = input("Name: ")
 
-guitar = Guitar("Gibson L-5 CES", 1922, 116035.40)
-print(guitar, guitar.is_vintage())
+print("These are my guitars")
+for guitar in guitars:
+    print(guitar)
