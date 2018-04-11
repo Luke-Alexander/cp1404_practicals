1a3cb633eccb7e5345124f409411856d2bceff70
Luke-Alexander <37579656+Luke-Alexander@users.noreply.github.com>
2018-04-11 12:30:01 +1000

    Add Prac 7

---
+++ prac_07/guitar.py
@@ -0,0 +1,19 @@
+class Guitar:
+    CURRENT_YEAR = 2018
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return self.CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        if self.get_age() >= 50:
+            return True
+        else:
+            return False
+
---
+++ prac_07/guitar_test.py
@@ -0,0 +1,4 @@
+from prac_07.guitar import Guitar
+
+guitar = Guitar("Gibson L-5 CES", 1922, 116035.40)
+print(guitar, guitar.is_vintage())
