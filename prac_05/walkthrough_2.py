4f43406c242f0a535129eb52868eb53540d9b0b2
Luke-Alexander <37579656+Luke-Alexander@users.noreply.github.com>
2018-04-13 11:13:24 +1000

    Add prac 4,5,6

---
+++ prac_05/walkthrough_2.py
@@ -0,0 +1,6 @@
+"""Luke Alexander---"""
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+
+for state in STATE_NAMES:
+    print('{:4}'.format(state), "is",STATE_NAMES[state] )
