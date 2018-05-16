4c9ef726c0f9b66a620c684600148cdf9e0c59b1
Luke-Alexander <37579656+Luke-Alexander@users.noreply.github.com>
2018-05-16 12:52:13 +1000

    Add Prac09

--- prac_09/sort_files.py
+++ prac_09/sort_files.py
@@ -2,13 +2,16 @@ import os
 import shutil
 
 os.chdir("FilesToSort")
-EXTENSIONS =[]
+extensions = []
 for directory_name, subdirectoris, filenames in os.walk("."):
     for filename in filenames:
         file = os.path.splitext(filename)
-        if file[1] not in EXTENSIONS:
-            EXTENSIONS.append(file[1])
-            print(EXTENSIONS)
+        if file[1] not in extensions:
+            extensions.append(file[1])
+            print(extensions)
+try:
+    for extension in extensions:
+        os.mkdir(extension[1:])
+except FileExistsError:
+    pass
 
-for extension in EXTENSIONS:
-    os.mkdir(extension)
