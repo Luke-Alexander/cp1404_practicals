2ec29c52c7681623ccc4591c8cd6dc75245ca325
Luke-Alexander <37579656+Luke-Alexander@users.noreply.github.com>
2018-05-24 11:22:57 +1000

    Turn broken_score and tempreture into functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,9 +1,15 @@
 score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+
+def get_result(score):
+    if score < 0 or score > 100:
+        return("Invalid score")
+    elif score >= 90:
+        return("Excellent")
+    elif score >= 50:
+        return("Passable")
+    else:
+        return("Bad")
+
+
+print(get_result(score))
--- prac_03/temperature.py
+++ prac_03/temperature.py
@@ -3,15 +3,23 @@ F – Convert Fahrenheit to Celsius
 Q – Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def get_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def get_celsius(fahrenheit):
+    return 5 / 9 * (fahrenheit - 32)
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("result: {:.2f} F".format(fahrenheit))
+        print("Result: {:.2f} C".format(get_fahrenheit(celsius)))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
+        print("Result: {:.2f} C".format(get_celsius(fahrenheit)))
     else:
         print("Invalid option")
     print(MENU)
